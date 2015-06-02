74b3b1c38ae8143d163977d592bf4ed005e42664
PhilippS92 <philippschlag92@googlemail.com>
2015-06-02 15:11:12 +0200

    Grundgerüst (loop) implementiert

--- kugelfall/kugelfall/kugelfall.ino
+++ kugelfall/kugelfall/kugelfall.ino
@@ -8,6 +8,19 @@
 #include <Servo.h> 
 Servo myservo;
 
+enum ControllerState {
+  IDLE,
+  WAIT_FOR_MEMORY,
+  WAIT_FOR_APPROX,
+  CALC_WAIT_DROP
+};
+
+enum MemoryState {
+  INIT,
+  NOT_READY,
+  READY
+};
+
 /**
  * Eine Datenstruktur, die einem konkreten Zeitpunkt einen
  * Wert zu ordnet.
@@ -23,9 +36,10 @@ struct timedValue {
  * Zahlen (Geschwindigkeit, Beschleunigung und Winkel) zu speichern.
  **/
 struct Memory {
-  bool isReady;
-  int photoCount;
-  timedValue photoValues[12]; //TODO; wie viele?
+  MemoryState state;
+  int photoLastIndex;
+  int photoCount; // 0 initial; 1-6: x-ter Photowert nach letztem Hallwert
+  timedValue photoValues[7]; //TODO; wie viele?
   timedValue hallValue; //TODO: wie viele?
   timedValue velocity[2]; //TODO: hier oder in Approximator???
   timedValue acceleration;
@@ -37,7 +51,7 @@ struct Memory {
  * nächstmöglichen Zeitpunkt zum Abwurf der Kugel zu speichern.
  **/
 struct Approximator {
-  bool isReady;
+  bool isValid;
   int nextDropTime;
 };
 
@@ -49,13 +63,16 @@ const int pin_out_servo = 9;
 const int pin_out_led1 = 12;
 const int pin_out_led2 = 13;
 
-int val = 0;
+int val_trigger = 0;
+int val_photo = 0;
+int val_hall = 0;
 int pos = 0;
 int time_start = 0;
 int time_end = 0;
 int time_diff;
 Memory defaultMemory;
 Approximator defaultApprox;
+ControllerState defaultControllerState;
 
 /**
  * Führe die Initialisierung aus.
@@ -72,7 +89,12 @@ void setup() {
   digitalWrite(pin_out_led1, LOW);
   digitalWrite(pin_out_led2, LOW);
   
-  //initialisiere den Wertespeicher
+  //initialisiere Controller
+  defaultControllerState = IDLE;
+  
+  //initialisiere den Wertespeicher und Approximator
+  initMemory();
+  initApproximator();
   
   //initialisiere den Motor
   myservo.attach(pin_out_servo);
@@ -85,23 +107,79 @@ void setup() {
  * Die Hauptschleife
  **/
 void loop() {
-  val = digitalRead(pin_in_trigger);
-  if(val == HIGH) {
-    drop();
+//  val = digitalRead(pin_in_trigger);
+//  if(val == HIGH) {
+//    drop();
+//  }
+  readInputs();
+  // TODO Update Memory
+  switch(defaultControllerState) {
+    case IDLE:
+      if(val_trigger == HIGH) {
+        defaultControllerState = WAIT_FOR_MEMORY;
+      } else {
+        break;
+      }
+    case WAIT_FOR_MEMORY:
+      if(defaultMemory.state == READY) {
+        defaultControllerState = WAIT_FOR_APPROX;
+      } else {
+        break;
+      }
+    case WAIT_FOR_APPROX:
+      // TODO APPROXIMIEREN-Aufruf
+      if(defaultApprox.isValid) {
+        defaultControllerState = CALC_WAIT_DROP;
+      } else {
+        break;
+      }
+    case CALC_WAIT_DROP:
+      // TODO calc drop time
+      // TODO wait for drop (and update) -> WAIT_FOR_APPROX?
+      // TODO drop -> defaultControllerState = IDLE;
+      break;
   }
+}
+
+/**
+ * Werte einlesen.
+ **/
+void readInputs() {
+  val_trigger = digitalRead(pin_in_trigger);
+  val_photo = digitalRead(pin_in_photo);
+  val_hall = digitalRead(pin_in_hall);
 }
 
 /**
  * Initialisiere den Wertespeicher.
  **/
 void initMemory() {
-  defaultMemory.isReady = false;
+  defaultMemory.state = INIT;
+  defaultMemory.photoLastIndex = 6;
+  defaultMemory.photoCount = 0;
   digitalWrite(pin_out_led1, LOW);
-  
+  for(int i=0; i<7; i++) {
+    defaultMemory.photoValues[i].time = -1;
+    //defaultMemory.photoValues[i].value = -1.0;
+  }
+  defaultMemory.hallValue.time = -1;
+  for(int i=0; i<2; i++) {
+    defaultMemory.velocity[i].time = -1;
+  }
+  defaultMemory.acceleration.time = -1;
+  defaultMemory.angle.time = -1;  
   //TODO
   
-  defaultMemory.isReady = true;
-  digitalWrite(pin_out_led1, HIGH);
+  //defaultMemory.isReady = true;
+  //digitalWrite(pin_out_led1, HIGH);
+}
+
+/**
+ * Initialisiere den Approximator.
+ **/
+void initApproximator() {
+  defaultApprox.isValid = false;
+  defaultApprox.nextDropTime = -1;
 }
 
 /**
