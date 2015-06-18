7a3de0289225fe7bfc37f2a25f68a31df75ef4fc
ckoecher <chris.koecher@tu-ilmenau.de>
2015-06-18 11:06:00 +0200

    - Überprüfung, ob Aufruf von approximate() notwendig ist, hinzugefügt - diverses Refactoring, Bugs und Tests

--- kugelfall/kugelfall/kugelfall.ino
+++ kugelfall/kugelfall/kugelfall.ino
@@ -83,6 +83,7 @@ unsigned long time_diff;
 Memory defaultMemory;
 Approximator defaultApprox;
 ControllerState defaultControllerState;
+boolean doApproximate = true;
 
 /**
  * Führe die Initialisierung aus.
@@ -134,12 +135,13 @@ void loop() {
 //    drop();
 //  }
   readInputs();
-  updateMemory();
+  boolean newValue = updateMemory();
   switch(defaultControllerState) {
     case IDLE:
       if(val_trigger == HIGH) {
         //defaultControllerState = WAIT_FOR_MEMORY;
         defaultControllerState = WAIT_APPROX;
+        doApproximate = true;
       } else {
         break;
       }
@@ -159,7 +161,9 @@ void loop() {
 //      }
     case WAIT_APPROX:
       if(defaultMemory.state == READY) {
-        approximate();
+        if(doApproximate || newValue) {
+          approximate();
+        }
         
         if(defaultApprox.isValid) {
           defaultControllerState = CALC_WAIT_DROP;
@@ -174,9 +178,12 @@ void loop() {
       
       if(busyWaitForDrop()) {
         drop();
+       // Serial.print("Geschwindigkeit: ");
+        Serial.println(defaultApprox.velocity);
         defaultControllerState = IDLE;
       } else {
         defaultControllerState = WAIT_APPROX;
+        doApproximate = true;
       }
       break;
   }
@@ -185,7 +192,7 @@ void loop() {
 /**
  * Debugging-Methode
  **/
-void printPhotoValues() {
+/*void printPhotoValues() {
   Serial.print("time: ");
   Serial.println(defaultMemory.photoValues[defaultMemory.photoLastIndex].time);
   Serial.print("photoCount: ");
@@ -195,10 +202,10 @@ void printPhotoValues() {
     if((defaultMemory.photoLastIndex+1+i)%maxNumPhotoValues < defaultMemory.photoTotalCount) {
       Serial.print(defaultMemory.photoValues[(defaultMemory.photoLastIndex+1+i)%maxNumPhotoValues].value);
       Serial.print(" - ");
-      Serial.print(defaultMemory.photoValues[(defaultMemory.photoLastIndex+1+i)%maxNumPhotoValues].time);
+      Serial.println(defaultMemory.photoValues[(defaultMemory.photoLastIndex+1+i)%maxNumPhotoValues].time);
     }
   }
-}
+}*/
 
 /**
  * Scheibenposition, Geschwindigkeit und Verzögerung approximieren
@@ -212,22 +219,21 @@ void approximate() {
   }
   
   // überprüfe, ob die Beschleunigung in Ordnung ist
-  float t1, t2, quot;
-  t2 = millis() - defaultMemory.photoValues[defaultMemory.photoLastIndex].time;
-  t1 = defaultMemory.photoValues[defaultMemory.photoLastIndex].time - defaultMemory.photoValues[(defaultMemory.photoLastIndex+maxNumPhotoValues-1)%maxNumPhotoValues].time;
-  quot = t2/t1;
-  if(quot > 1.5) {//Serial.print(millis());Serial.print(" ");Serial.print(defaultMemory.photoValues[defaultMemory.photoLastIndex].time);Serial.print(" ");Serial.println(defaultMemory.photoValues[(defaultMemory.photoLastIndex+maxNumPhotoValues-1)%maxNumPhotoValues].time);
+  if(!validateSpeedUp()) {
     // der tatsächliche quot-Wert des aktuellen Abschnittes kann nur größer sein
     // plötzlicher Stopp innerhalb eines 30°-Segmentes wird erkannt
     defaultApprox.isValid = false;
     digitalWrite(pin_out_led2, LOW);
     return;
   }
+  float t1, t2, quot;
+  t1 = (float)defaultMemory.photoValues[defaultMemory.photoLastIndex].time - (float)defaultMemory.photoValues[(defaultMemory.photoLastIndex+maxNumPhotoValues-1)%maxNumPhotoValues].time;
   for(int i = 1; i < maxNumPhotoValues-1; i++) {
     t2 = t1;
-    t1 = defaultMemory.photoValues[(defaultMemory.photoLastIndex+maxNumPhotoValues-i)%maxNumPhotoValues].time - defaultMemory.photoValues[(defaultMemory.photoLastIndex+maxNumPhotoValues-i-1)%maxNumPhotoValues].time;
+    t1 = (float)defaultMemory.photoValues[(defaultMemory.photoLastIndex+maxNumPhotoValues-i)%maxNumPhotoValues].time - (float)defaultMemory.photoValues[(defaultMemory.photoLastIndex+maxNumPhotoValues-i-1)%maxNumPhotoValues].time;
     quot = t2/t1;
-    if(quot < 0.5 || quot > 1.5) {Serial.print("bla3 ");Serial.println(quot);
+    //Serial.println(quot);
+    if(quot < 0.5 || quot > 1.5) {//Serial.print("bla3 ");Serial.println(quot);//printPhotoValues();
       defaultApprox.isValid = false;
       digitalWrite(pin_out_led2, LOW);
       return;
@@ -252,13 +258,15 @@ void approximate() {
   // Verzögerung
   //  defaultApprox.acceleration = ((float)defaultMemory.photoValues[defaultMemory.photoLastIndex].time + (float)defaultMemory.photoValues[(defaultMemory.photoLastIndex+maxNumPhotoValues-6)%maxNumPhotoValues].time - 2*(float)defaultMemory.photoValues[(defaultMemory.photoLastIndex+maxNumPhotoValues-3)%maxNumPhotoValues].time) / 3.0;
   defaultApprox.acceleration = ((float)defaultMemory.photoValues[defaultMemory.photoLastIndex].time + (float)defaultMemory.photoValues[(defaultMemory.photoLastIndex+maxNumPhotoValues-12)%maxNumPhotoValues].time - 2*(float)defaultMemory.photoValues[(defaultMemory.photoLastIndex+maxNumPhotoValues-6)%maxNumPhotoValues].time) / 6.0;
-  if(defaultApprox.acceleration < 0 || defaultApprox.acceleration > 12345) {Serial.print("bla5");Serial.println(defaultApprox.acceleration);
+  if(/*defaultApprox.acceleration < -5 ||*/ defaultApprox.acceleration > 12345) {//Serial.print("bla5");Serial.println(defaultApprox.acceleration);
     defaultApprox.isValid = false;
     digitalWrite(pin_out_led2, LOW);
     Serial.print("bad acceleration: ");
     Serial.println(defaultApprox.acceleration);
-    printPhotoValues();
+//    printPhotoValues();
     return;
+  } else if(defaultApprox.acceleration < 0) {
+    defaultApprox.acceleration = 0;
   }
   
   
@@ -266,6 +274,19 @@ void approximate() {
   defaultApprox.time = defaultMemory.photoValues[defaultMemory.photoLastIndex].time;
   defaultApprox.isValid = true;
   digitalWrite(pin_out_led2, HIGH);
+  doApproximate = false;
+}
+
+boolean validateSpeedUp() {
+  float t1, t2, quot;
+  t2 = (float)millis() - (float)defaultMemory.photoValues[defaultMemory.photoLastIndex].time;
+  t1 = (float)defaultMemory.photoValues[defaultMemory.photoLastIndex].time - (float)defaultMemory.photoValues[(defaultMemory.photoLastIndex+maxNumPhotoValues-1)%maxNumPhotoValues].time;
+  quot = t2/t1;
+//  if(quot > 1.2) {
+//    Serial.print(millis());Serial.print(" ");Serial.print(defaultMemory.photoValues[defaultMemory.photoLastIndex].time);Serial.print(" ");Serial.println(defaultMemory.photoValues[(defaultMemory.photoLastIndex+maxNumPhotoValues-1)%maxNumPhotoValues].time);
+//    Serial.println(quot);
+//  }
+  return quot <= 1.5;
 }
 
 /**
@@ -279,7 +300,7 @@ void calcDropTime() {
     n += 12;
   } while(tmin < current + timeDelay);
   defaultApprox.nextDropTime = tmin - timeDelay;
-  Serial.println(defaultApprox.acceleration);
+  //Serial.println(defaultApprox.acceleration);
 }
 
 /**
@@ -288,6 +309,7 @@ void calcDropTime() {
 void readInputs() {
   val_millis = millis();
   val_trigger = digitalRead(pin_in_trigger);
+//  val_trigger = HIGH;
   val_photo = digitalRead(pin_in_photo);
   val_hall = digitalRead(pin_in_hall);
 }
@@ -299,6 +321,7 @@ void initApproximator() {
   defaultApprox.isValid = false;
   defaultApprox.time = 0;
   defaultApprox.nextDropTime = 0;
+  doApproximate = true;
 }
 
 /**
@@ -311,7 +334,7 @@ void initMemory() {
   defaultMemory.photoTotalCount = 0;
   digitalWrite(pin_out_led1, LOW);
   for(int i=0; i<maxNumPhotoValues; i++) {
-    defaultMemory.photoValues[i].time = 0;
+    defaultMemory.photoValues[i].time = 0;        
     defaultMemory.photoValues[i].value = -1;
   }
   defaultMemory.hallValue.time = millis();
@@ -413,10 +436,18 @@ boolean busyWaitForDrop() {
     readInputs();
     if(updateMemory()) {
       approximate();
-      if(!defaultApprox.isValid) {Serial.println("bla");
+      if(!defaultApprox.isValid) {//Serial.println("bla");
         return false;
       }
       calcDropTime();
+    } else {
+      if(!validateSpeedUp()) {//Serial.print(millis());Serial.print(" ");Serial.print(defaultMemory.photoValues[defaultMemory.photoLastIndex].time);Serial.print(" ");Serial.println(defaultMemory.photoValues[(defaultMemory.photoLastIndex+maxNumPhotoValues-1)%maxNumPhotoValues].time);
+        // der tatsächliche quot-Wert des aktuellen Abschnittes kann nur größer sein
+        // plötzlicher Stopp innerhalb eines 30°-Segmentes wird erkannt
+        defaultApprox.isValid = false;
+        digitalWrite(pin_out_led2, LOW);
+        return false;
+      }
     }
   }
 }
